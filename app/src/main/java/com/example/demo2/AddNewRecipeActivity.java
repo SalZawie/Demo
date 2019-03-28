4bfb90608d258b8661bc57f915129d6afc7ec411
stjeandan-swc <stjean.dan.dev@gmial.com>
2019-03-28 16:14:28 -0400

    Implemented UI. Added dependency "com.android.support:design" to gradle file.

---
+++ app/src/main/java/com/example/demo2/AddNewRecipeActivity.java
@@ -0,0 +1,16 @@
+package com.example.demo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewRecipeActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_recipe);
+
+    }
+}
