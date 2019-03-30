90a5f525f0433cf7dd53930e53333f0afb3c03f4
La Kuch <la.kuch@students.williscollege.com>
2019-03-29 20:08:08 -0400

    Recipe Page

--- app/src/main/java/com/example/demo2/MainActivity.java
+++ app/src/main/java/com/example/demo2/MainActivity.java
@@ -9,31 +9,31 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button submitButton;
-    private EditText name;
+    private EditText nameEditText;
+    private EditText passwordEditText;
     private String strName;
     private String strPassword;
-    private EditText pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        submitButton = findViewById(R.id.btnSubmit);
-        name = findViewById(R.id.name);
-        pw   = findViewById(R.id.pw);
+        submitButton = findViewById(R.id.submitButton);
+        nameEditText = findViewById(R.id.nameEditText);
+        passwordEditText   = findViewById(R.id.passwordEditText);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                strName = name.getText().toString().trim();
-                strPassword = pw.getText().toString().trim();
+            strName = nameEditText.getText().toString().trim();
+            strPassword = passwordEditText.getText().toString().trim();
 
-                if (!strName.isEmpty() && !strPassword.isEmpty()) {
-                    Intent intent = new Intent(MainActivity.this, HelloPage.class);
-                    startActivity(intent);
-                }
+            if (!strName.isEmpty() && !strPassword.isEmpty()) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
             }
         });
     }
