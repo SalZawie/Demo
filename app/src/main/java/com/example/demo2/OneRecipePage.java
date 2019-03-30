d4521b201d8e222d1c68f5067ef29b9ace07589a
Oksana Miller <milleroks1992@gmail.com>
2019-03-30 16:56:52 -0400

    Single Recipe Page

--- app/src/main/java/com/example/demo2/OneRecipePage.java
+++ app/src/main/java/com/example/demo2/OneRecipePage.java
@@ -1,5 +1,6 @@
 package com.example.demo2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,21 @@ public class OneRecipePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_recipe_page);
+    }
+
+    public void back()
+    {
+
+    }
+
+    public void logout()
+    {
+        Intent intent = new Intent(OneRecipePage.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void add()
+    {
+
     }
 }
