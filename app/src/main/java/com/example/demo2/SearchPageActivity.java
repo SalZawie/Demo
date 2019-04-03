5da1b3febb1e5d7cdbbd9b8ee648a063b0b6eb12
HasAlq <alqassimi_hassan@hotmail.com>
2019-04-03 16:14:57 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/demo2/SearchPageActivity.java
@@ -0,0 +1,64 @@
+package com.example.demo2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SearchPageActivity extends AppCompatActivity {
+    private EditText searchOne;
+    private EditText searchTwo;
+    private EditText searchThree;
+
+    private Button backButton;
+    private Button logoutButton;
+    private Button addRecipeButton;
+    private Button searchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_page);
+
+        searchOne = findViewById(R.id.searchOne);
+        searchTwo = findViewById(R.id.searchTwo);
+        searchThree = findViewById(R.id.searchThree);
+        searchButton = findViewById(R.id.searchButton);
+        backButton = findViewById(R.id.backButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        addRecipeButton = findViewById(R.id.addRecipeButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchPageActivity.this, RecipePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchPageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchPageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        addRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchPageActivity.this, AddNewRecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
