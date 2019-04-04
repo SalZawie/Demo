59648d806338fd0382aba6505221d1957f8282b5
HasAlq <alqassimi_hassan@hotmail.com>
2019-04-04 15:08:38 -0400

    SearchPage

--- app/src/main/java/com/example/demo2/SearchPageActivity.java
+++ app/src/main/java/com/example/demo2/SearchPageActivity.java
@@ -1,16 +1,25 @@
 package com.example.demo2;
 
 import android.content.Intent;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class SearchPageActivity extends AppCompatActivity {
     private EditText searchOne;
     private EditText searchTwo;
     private EditText searchThree;
+
+    private CheckBox foodCheckBox;
+    private CheckBox drinkCheckBox;
+
+    private String firstIngredient;
+    private String secondIngredient;
+    private String thirdIngredient;
 
     private Button backButton;
     private Button logoutButton;
@@ -29,11 +38,20 @@ public class SearchPageActivity extends AppCompatActivity {
         backButton = findViewById(R.id.backButton);
         logoutButton = findViewById(R.id.logoutButton);
         addRecipeButton = findViewById(R.id.addRecipeButton);
+        foodCheckBox = findViewById(R.id.foodCheckBox);
+        drinkCheckBox = findViewById(R.id.drinkCheckBox);
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                firstIngredient = searchOne.getText().toString().toLowerCase();
+                secondIngredient = searchTwo.getText().toString().toLowerCase();
+                thirdIngredient = searchThree.getText().toString().toLowerCase();
+
+
+
                 Intent intent = new Intent(SearchPageActivity.this, RecipePageActivity.class);
+                intent.putExtra("Ingredients", new String[]{firstIngredient, secondIngredient, thirdIngredient});
                 startActivity(intent);
             }
         });
