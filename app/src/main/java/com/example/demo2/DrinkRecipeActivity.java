94a568b9224aff08caab3529ed5ba3af5d0cb55c
thomas-mayfield-swc <thomas.mayfield@students.williscollege.com>
2019-04-05 16:33:16 -0400

    Drinks java files

---
+++ app/src/main/java/com/example/demo2/DrinkRecipeActivity.java
@@ -0,0 +1,48 @@
+package com.example.demo2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DrinkRecipeActivity extends AppCompatActivity {
+
+    private Button backButton;
+    private Button logoutButton;
+    private Button addRecipeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_recipe);
+        backButton = findViewById(R.id.backButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        addRecipeButton = findViewById(R.id.addRecipeButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DrinkRecipeActivity.this, DrinksPage.class); //Replace MainActivity.class with OneRecipePage.class
+                startActivity(intent);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DrinkRecipeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DrinkRecipeActivity.this, AddNewRecipeActivity.class); //Replace MainAcitivity.class with AddNewRecipeActivity.class
+                startActivity(intent);
+            }
+        });
+    }
+
+}
