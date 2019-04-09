2d7a26bd28488e1a1b21de161c379a40546915e8
stjeandan-swc <stjean.dan.dev@gmail.com>
2019-04-09 13:48:00 -0400

    Rewrote add new recipe code. fixed UI. Added layout file newfield.xml. Wrote code for recipe database.

--- app/src/main/java/com/example/demo2/AddNewRecipeActivity.java
+++ app/src/main/java/com/example/demo2/AddNewRecipeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +13,15 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AddNewRecipeActivity extends AppCompatActivity
 {
-
     private LinearLayout mLinearLayout;
 
     private EditText mRecipeName;
     private EditText mEnterRecipe;
-    private List<EditText> mIngredientList;
-    private int editTextCounter = 1;
+
+    private int editTextCounter = 0;
     private static final int MAX_EDITTEXT_LIMIT = 20;
 
     private Button mAddIngredientBtn;
@@ -40,12 +39,10 @@ public class AddNewRecipeActivity extends AppCompatActivity
         // UI elements
         mRecipeName = findViewById(R.id.edittxt_recipe_name);
         mEnterRecipe = findViewById(R.id.edittxt_recipe_description);
-        mIngredientList = new ArrayList<>();
 
         // Instantiate Buttons
         mAddIngredientBtn = findViewById(R.id.btn_add_an_ingredient);
         mBackBtn = findViewById(R.id.btn_previous_from_add_new_recipe);
-        mClearAllBtn = findViewById(R.id.btn_clear_all_input);
         mSaveRecipeBtn = findViewById(R.id.btn_save_recipe);
     }
 
@@ -57,15 +54,16 @@ public class AddNewRecipeActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
 
-                if(editTextCounter >= MAX_EDITTEXT_LIMIT)
-                {
-                    Toast.makeText(AddNewRecipeActivity.this, "You have reached max ingredients", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    addNewEditTextField();
-                    editTextCounter++;
-                }
+            if(editTextCounter >= MAX_EDITTEXT_LIMIT)
+            {
+                Toast toast = Toast.makeText(AddNewRecipeActivity.this, "You have reached max ingredients", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+            else
+            {
+                addNewEditTextField();
+            }
             }
         });
 
@@ -84,31 +82,76 @@ public class AddNewRecipeActivity extends AppCompatActivity
             @Override
             public void run() {
 
-                try
-                {
-                    LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            try
+            {
+                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-                    final View rowView = inflater.inflate(R.layout.newfield, null);
+                final View rowView = inflater.inflate(R.layout.newfield, null);
 
-                    assert mLinearLayout != null;
-                    mLinearLayout.post(new Runnable() {
-                        @Override
-                        public void run() {
+                assert mLinearLayout != null;
+                mLinearLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
 
-                            mLinearLayout.addView(rowView, mLinearLayout.getChildCount() - 1);
-                        }
-                    });
-                }
-                catch (NullPointerException ex)
-                {
-                    ex.printStackTrace();
-                }
+                        mLinearLayout.addView(rowView, mLinearLayout.getChildCount());
+                    }
+                });
+            }
+            catch (NullPointerException ex)
+            {
+                ex.printStackTrace();
+            }
             }
         });
         extraThread.start();
     }
 
-    public void onDelete(View v) {
+
+    public ArrayList<String> getAllIngredients()
+    {
+        ArrayList<LinearLayout> linearList = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        // Adds all children linearlayouts to a list.
+        for(int i = 0; i < mLinearLayout.getChildCount(); i++)
+        {
+            if(mLinearLayout.getChildAt(i) instanceof LinearLayout)
+            {
+                linearList.add((LinearLayout) mLinearLayout.getChildAt(i));
+            }
+        }
+
+        // Targets the edittext fields within the list of linear layouts and get user input.
+        for(int i = 0; i < linearList.size(); i++)
+        {
+            LinearLayout l = linearList.get(i);
+            EditText et = (EditText) l.getChildAt(0);
+
+            if(et.getText().toString() != null || !et.getText().toString().isEmpty())
+            {
+                list.add(et.getText().toString());
+            }
+        }
+        return list;
+    }
+
+    public void onSaveData(View view)
+    {
+        ArrayList<String> ingredientsList = new ArrayList<>();
+        ingredientsList = getAllIngredients();
+
+        Toast.makeText(AddNewRecipeActivity.this, "Recipe Has Been Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClearAll(View view)
+    {
+        mRecipeName.getText().clear();
+        mEnterRecipe.getText().clear();
+
+    }
+
+    public void onDelete(View v)
+    {
         mLinearLayout.removeView((View) v.getParent());
     }
 }
--- app/src/main/java/com/example/demo2/BDRecipesModel.java
+++ app/src/main/java/com/example/demo2/BDRecipesModel.java
@@ -1,20 +1,29 @@
 package com.example.demo2;
 
-import android.widget.EditText;
 import java.util.ArrayList;
 
 public class BDRecipesModel
 {
+    private int mRecipeId;
     private String mRecipeName;
     private String mRecipeDescription;
-    private ArrayList<EditText> mIngredientsList;
+    private ArrayList<String> mIngredientsList = new ArrayList<String>();
 
-    public BDRecipesModel(String recipeName, String recipeDescription, ArrayList<EditText> ingredientsList)
+    public BDRecipesModel()
     {
+
+    }
+
+    public BDRecipesModel(int id, String recipeName, String recipeDescription, ArrayList<String> ingredientsList)
+    {
+        this.mRecipeId = id;
         this.mRecipeName = recipeName;
         this.mRecipeDescription = recipeDescription;
         this.mIngredientsList = ingredientsList;
     }
+
+    public void setRecipeId(int id) { this.mRecipeId = id; }
+    public int getRecipeId() { return this.mRecipeId; }
 
     public void setRecipeName(String recipeName) { this.mRecipeName = recipeName; }
     public String getRecipeName() { return this.mRecipeName; }
@@ -22,6 +31,6 @@ public class BDRecipesModel
     public void setRecipeDescription(String recipeDescription) { this.mRecipeDescription = recipeDescription; }
     public String getRecipeDescription() { return this.mRecipeDescription; }
 
-    public void setIngredientsList(ArrayList<EditText> ingredientsList) { this.mIngredientsList = ingredientsList; }
-    public ArrayList<EditText> getIngredientsList() { return this.mIngredientsList; }
+    public void setIngredientsList(ArrayList<String> ingredientsList) { this.mIngredientsList = ingredientsList; }
+    public ArrayList<String> getIngredientsList() { return this.mIngredientsList; }
 }
