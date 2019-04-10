487a5eb0bfb9d5549397dbada71df38e10bc3e68
La Kuch <la.kuch@students.williscollege.com>
2019-04-10 14:03:40 -0400

    Changed Back button, goes to SearchPage instead of Main

--- app/src/main/java/com/example/demo2/RecipePageActivity.java
+++ app/src/main/java/com/example/demo2/RecipePageActivity.java
@@ -55,7 +55,7 @@ public class RecipePageActivity extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RecipePageActivity.this, MainActivity.class);
+                Intent intent = new Intent(RecipePageActivity.this, SearchPageActivity.class);
                 startActivity(intent);
             }
         });
