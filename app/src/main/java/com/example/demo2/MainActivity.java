3d20a41b5cf2804c3a79634d0eb8b0305c3c3aa8
SZawie <zswhatever@gmail.com>
2019-04-12 13:23:16 -0400

    Login & SignUP Code Fixes..

--- app/src/main/java/com/example/demo2/MainActivity.java
+++ app/src/main/java/com/example/demo2/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.demo2;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.regex.Pattern;
 
@@ -25,8 +21,8 @@ public class MainActivity extends AppCompatActivity
                                          ".{6,}" +            //at least 6 characters
                                          "$");                //End of Expression
 
-    private EditText userEmail;
-    private EditText passWord;
+    private TextInputLayout userEmail;
+    private TextInputLayout passWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,8 +30,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userEmail = (EditText) findViewById(R.id.txtEmail);
-        passWord  = (EditText) findViewById(R.id.txtPassword);
+        userEmail = (TextInputLayout) findViewById(R.id.txtEmail);
+        passWord  = (TextInputLayout) findViewById(R.id.txtPassword);
     }
 
     public void validateEmailPassword(View v)
@@ -51,7 +47,7 @@ public class MainActivity extends AppCompatActivity
 
     private boolean validateEmail()
     {
-        String emailInput = userEmail.getText().toString().trim();
+        String emailInput = userEmail.getEditText().getText().toString().trim();
 
         if (emailInput.isEmpty())
         {
@@ -72,7 +68,7 @@ public class MainActivity extends AppCompatActivity
 
     private boolean validatePassword()
     {
-        String passwordInput = passWord.getText().toString().trim();
+        String passwordInput = passWord.getEditText().getText().toString().trim();
 
         if (passwordInput.isEmpty())
         {
