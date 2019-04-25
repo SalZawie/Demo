2b0ea6743831eab7013dafb0abade5355f6926d5
SZawie <zswhatever@gmail.com>
2019-04-25 15:38:32 -0400

    new push

--- app/src/main/java/com/example/demo2/MainActivity.java
+++ app/src/main/java/com/example/demo2/MainActivity.java
@@ -10,12 +10,10 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.regex.Pattern;
 
 public class MainActivity extends LoginMenu
